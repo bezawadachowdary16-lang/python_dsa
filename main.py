aa266b6ff5241d8a94cac994b47d1c75f6aa1093
venu <venugopalgottimukkala99@gmail.com>
2026-07-20 12:05:29 +0530

    day1

---
+++ main.py
@@ -0,0 +1,22 @@
+#print("Veeraswamy\nVeeraswamy")
+#print("r\tcurent\new\folder")
+# / is a escape character
+
+# def check(num):
+#     if num%2==0:
+#         print("even")
+#     else:
+#         print("odd")
+# check(10)
+
+# dict={
+#     "name":"chowdary",
+#     "gender":"male",
+#     "age":"23",
+#     "courses":["python","java"]
+# }
+# print(dict)   
+# while True:
+#     print("hi")
+
+
