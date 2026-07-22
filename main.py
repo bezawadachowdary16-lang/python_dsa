bc024c062d40844418eff7f1aa9bdc1140a0e358
dubavenkatesh50-sys <dubavenkatesh50@gmal.com>
2026-07-22 11:54:13 +0530

    Day 3

--- main.py
+++ main.py
@@ -1,51 +1,50 @@
-print("veeraswamy\n"*3)
-print(r"\tcurrent\new\folder")
-def function(n):
-    if n%2==0:
-        print("Even")
-    else:
-        print("Odd")
-n= int(input("Enter a number: "))
-function(n)
-d={"name":"veeraswamy","age":20,"Town":"Vijayawada"}
-d.update({"name":"Sai"})
-# print(d)
-dict={
-    "name":"veeraswamy",
-    "gender":"male",
-    "age":"20",
-    "courses":["Python","Java","Data Science"]
-}
-dict.update({"name":"Hari"})
-dict.update({"courses":["Machine Learning","Deep Learning","C++","C"]})
-print(dict)
-def count(*args):
-    print(type(args))
-# count(1,2,3,4,5,6,7,8,9)
-def dicts(**kwargs):
-    print(type(kwargs))
-dicts(name="veeraswamy",age=21,Gender="Male")
-oops
-l=[1,2,3]
-s="String"
-len(l)
-len(s)
-print(len(l))
-print(len(s))
-x="veeraswamy"
-x=x[1:5]
-x=x[::-1]
-print(x)
-while True:
-    print("Hi")
-n=30
-sum=0
-while n>0:
-    sum+=n
-    n-=1
-print(sum)
-n=30
-sum=0
-for i in range(1,n+1):
-    sum+=i
-print("Sum of first",n,"numbers is:",sum)
+# print("veeraswamy\n"*3)
+# print(r"\tcurrent\new\folder")
+# def function(n):
+#     if n%2==0:
+#         print("Even")
+#     else:
+#         print("Odd")
+# n= int(input("Enter a number: "))
+# function(n)
+# d={"name":"veeraswamy","age":20,"Town":"Vijayawada"}
+# d.update({"name":"Sai"})
+# # print(d)
+# dict={
+#     "name":"veeraswamy",
+#     "gender":"male",
+#     "age":"20",
+#     "courses":["Python","Java","Data Science"]
+# }
+# dict.update({"name":"Hari"})
+# dict.update({"courses":["Machine Learning","Deep Learning","C++","C"]})
+# print(dict)
+# def count(*args):
+#     print(type(args))
+# # count(1,2,3,4,5,6,7,8,9)
+# def dicts(**kwargs):
+#     print(type(kwargs))
+# dicts.update(name="veeraswamy",age=21,Gender="Male")
+# l=[1,2,3]
+# s="String"
+# len(l)
+# len(s)
+# print(len(l))
+# print(len(s))
+# x="veeraswamy"
+# x=x[1:5]
+# x=x[::-1]
+# print(x)
+# while True:
+#     print("Hi")
+# n=30
+# sum=0
+# while n>0:
+#     sum+=n
+#     n-=1
+# print(sum)
+# n=30
+# sum=0
+# for i in range(1,n+1):
+#     sum+=i
+# print("Sum of first",n,"numbers is:",sum)
---
+++ patterns.py
@@ -0,0 +1,58 @@
+# n=5
+# for i in range(n):
+#     print("*")
+
+# n=5
+# for i in range(n):
+#     print("*",end=" ")
+
+# n=5
+# for i in range(n):
+#     for j in range(n):
+#         print("*",end=" ")
+#     print()
+
+#right angled triangle
+# n=5
+# for i in range(n):
+#     for j in range(i+1):
+#         print("*",end=" ")
+#     print()
+#inverted right angle triangle
+# n=5
+# for i in range(1+n):
+#     for j in range(i-1,n):
+#         print("*",end=" ")
+#     print()
+
+#diamond pattern
+# n=5
+# for i in range(n):
+#     for j in range(n-i-1):
+#         print("",end=" ")
+#     for j in range(2*i+1):
+#         print("*",end="")
+#     print()
+# for i in range(n-2,-1,-1):
+#     for j in range(n-i-1):
+#         print("",end=" ")
+#     for j in range(2*i+1):
+#         print("*",end="")
+#     print()
+
+#Armstrong Number
+# 
+
+#Hallow square pattern
+# n=int(input("Enetr a number:"))
+# for i in range(n):
+#     for j in range(n):
+#         if i==0 or i==n-1 or j==0 or j==n-1:
+#             print("*",end=" ")
+#         else:
+#             print(" ",end=" ")
+#     print()
+
+#pascals paterns
+
+    
